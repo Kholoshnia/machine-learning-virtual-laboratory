7e7b2d848f0a370436487fe31b106f40bc81eb51
Vadim Kholoshnya <vhd-ru@yandex.ru>
2019-12-31 14:35:57 +0300

    Add MacOS NEAT files

--- Source Files/MacOS x64/Algorithms/NEAT/NEAT/main.cpp
+++ Source Files/MacOS x64/Algorithms/NEAT/NEAT/main.cpp
@@ -1,21 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <fstream>
+#include <memory>
+#include <vector>
+
+#include <Enums.h>
+
+#include "Map.h"
+#include "Layers.h"
+#include "Population.h"
+
+Modes mode;
+
+std::string path;
+
+sf::Font font;
+sf::Image icon;
+sf::Sprite loading;
+sf::Texture loading_texture;
+std::string path_input, path_output;
+
+bool from_image, check_from_file, map_loaded, result_loaded, output_path_set, pause, show_controls;
+
+VisualizationTypes visualization_type;
+
+std::shared_ptr<Map> map;
+std::shared_ptr<Layers> layers;
+std::shared_ptr<Population> population;
+
+std::fstream fout, fin;
+std::vector<sf::Vector2f> pos;
+std::vector<std::string> map_markup;
+
+sf::RectangleShape rect;
+sf::Text text[4], controls[3];
+sf::Vector2i map_size, wall_size, pos_agent, pos_goal;
+
+bool around, map_changed;
+float radius_agent, radius_goal, max_speed, mutation_rate;
+unsigned int width, height, directions_array_size, population_quantity, layers_quantity, auto_exit;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
- 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-
-        window.display();
-    }
     
     return 0;
 }
